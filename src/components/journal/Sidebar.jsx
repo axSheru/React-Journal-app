62b618d7ab8288b401a751941d00bcc0d8f5b7e0
Alex Paredes <alexparedesr95@gmail.com>
2022-03-22 16:58:44 -0600

    FEAT: Created and imported Sidebar component.

---
+++ src/components/journal/Sidebar.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export const Sidebar = () => {
+    return (
+        <aside className='journal__sidebar'>
+            <div className="journal__sidebar-navbar">
+                <h3 className='mt-5'>
+                    <i className='fa fa-moon' ></i>
+                    <span> Alex</span>
+                </h3>
+
+                <button className='btn'>Logout</button>
+            </div>
+            <div className="journal___new-entry">
+                <i className='far fa-calendar-plus fa-5x'></i>
+                <p className='mt-5'>New entry</p>
+            </div>
+        </aside>
+    );
+}
