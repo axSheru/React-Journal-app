c83b93deeef91cc0f278e4c901436092a45de82e
Alex Paredes <alexparedesr95@gmail.com>
2022-03-21 22:06:07 -0600

    FEAT: Added classNames and used Navigate to redirect.

--- src/routers/AuthRouter.jsx
+++ src/routers/AuthRouter.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import { LoginScreen } from '../components/auth/LoginScreen';
 import { RegisterScreen } from '../components/auth/RegisterScreen';
 
 export const AuthRouter = () => {
     return (
-        <Routes>
-            <Route path='login' element={ <LoginScreen /> } />
-            <Route path='register' element={ <RegisterScreen /> } />
-            <Route path='*' element={ <LoginScreen /> } />
-        </Routes>
+        <div className='auth__main'>
+            <div className='auth__box-container'>
+                <Routes>
+                    <Route path='login' element={ <LoginScreen /> } />
+                    <Route path='register' element={ <RegisterScreen /> } />
+                    <Route path='*' element={ <Navigate to='login' replace /> } />
+                </Routes>
+            </div>
+        </div>
     );
 }
