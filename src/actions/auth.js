ea507d452af8d49565c950e52eff6f022e650629
Alex Paredes <alexparedesr95@gmail.com>
2022-03-23 12:47:29 -0600

    FEAT: Created async action startLoginEmailPassword.

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,5 +1,11 @@
 import { types } from "../types/types";
 
+export const startLoginEmailPassword = ( email, password ) => {
+    return ( dispatch ) => {
+        setTimeout(() => { dispatch( login( 1234, 'Roberto' ) ) }, 3500 );
+    };
+};
+
 export const login = ( uid, displayName ) => {
     return {
         type: types.login,
