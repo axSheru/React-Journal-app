c95b032f57b79f75f42b99471fd96cfef5a3d82d
Alex Paredes <alexparedesr95@gmail.com>
2022-03-23 13:44:31 -0600

    FEAT: Implemented useForm hook and created handleRegister function to handle register form.

--- src/components/auth/RegisterScreen.jsx
+++ src/components/auth/RegisterScreen.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useForm } from '../../hooks/useForm';
 
 export const RegisterScreen = () => {
+
+    const [ formValues, handleInputChange ] = useForm({
+        name: 'Alex P',
+        email: 'alexp@test.com',
+        password: '12345678',
+        password2: '12345678'
+    });
+
+    const { name, email, password, password2 } = formValues;
+
+    const handleRegister = ( e ) => {
+        e.preventDefault();
+        console.table([ name, email, password, password2 ]);
+    };
+
     return (
         <>
             <h3 className='auth__title'>Register</h3>
@@ -13,6 +29,8 @@ export const RegisterScreen = () => {
                     name="name"
                     autoComplete='off'
                     className='auth__input'
+                    value={ name }
+                    onChange={ handleInputChange }
                 />
                 <input
                     type="text"
@@ -20,22 +38,29 @@ export const RegisterScreen = () => {
                     name="email"
                     autoComplete='off'
                     className='auth__input'
+                    value={ email }
+                    onChange={ handleInputChange }
                 />
                 <input
                     type="password"
                     placeholder='Password'
                     name="password"
                     className='auth__input'
+                    value={ password }
+                    onChange={ handleInputChange }
                 />
                 <input
                     type="password"
                     placeholder='Confirm password'
                     name="password2"
                     className='auth__input'
+                    value={ password2 }
+                    onChange={ handleInputChange }
                 />
                 <button
                     type='submit'
                     className='btn btn-primary btn-block mb-5'
+                    onClick={ handleRegister }
                 >
                     Register
                 </button>
