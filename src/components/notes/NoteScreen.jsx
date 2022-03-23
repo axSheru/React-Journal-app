18609a1a7c55574a809de1cd42c02ccea32f68aa
Alex Paredes <alexparedesr95@gmail.com>
2022-03-22 19:27:36 -0600

    FEAT: Added structure to NoteScreen component.

--- src/components/notes/NoteScreen.jsx
+++ src/components/notes/NoteScreen.jsx
@@ -5,6 +5,27 @@ export const NoteScreen = () => {
     return (
         <div className='notes__main-content'>
             <NotesAppBar />
+
+            <div className="notes__content">
+                <form className='notes__main-content'>
+                    <input
+                        type="text"
+                        placeholder='Some awesome title'
+                        className='notes__title-input'
+                        autoComplete='off'
+                    />
+                    <textarea
+                        placeholder='What happened today?'
+                        className='notes__textarea'
+                    ></textarea>
+                    <div className="notes__image">
+                        <img
+                            src="https://i1.wp.com/www.lafosadelrancor.com/wp-content/uploads/2019/07/Sith-Trooper-El-Ascenso-de-Skywalker.jpg?fit=678%2C489&ssl=1"
+                            alt="imagen"
+                        />
+                    </div>
+                </form>
+            </div>
         </div>
     );
 }
