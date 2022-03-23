e97833db9343a54bf52b5aaa36bba359a9a00869
Alex Paredes <alexparedesr95@gmail.com>
2022-03-23 13:19:07 -0600

    FEAT: Created startGoogleLogin action.

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,8 +1,20 @@
+import { firebase, googleAuthProvider } from '../firebase/firebase-config';
 import { types } from "../types/types";
 
 export const startLoginEmailPassword = ( email, password ) => {
     return ( dispatch ) => {
         setTimeout(() => { dispatch( login( 1234, 'Roberto' ) ) }, 3500 );
+    };
+};
+
+export const startGoogleLogin = () => {
+    return ( dispatch ) => {
+
+        firebase.auth().signInWithPopup( googleAuthProvider )
+            .then( ({ user }) => {
+                dispatch( login( user.uid, user.displayName ) );
+            });
+
     };
 };
 
