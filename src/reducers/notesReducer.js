fe842c88eba592b3a51c3d125c3d5ffb5e065b73
Alex Paredes <alexparedesr95@gmail.com>
2022-03-24 15:23:27 -0600

    FEAT: Created notesReducer to handle notes state.

---
+++ src/reducers/notesReducer.js
@@ -0,0 +1,14 @@
+const initialstate = {
+    notes: [],
+    active: null
+};
+
+export const notesReducer = ( state = initialstate, action ) => {
+
+    switch ( action.type ) {
+    
+        default:
+            return state;
+    }
+
+};
