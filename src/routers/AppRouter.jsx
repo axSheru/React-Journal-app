4ca69586cb3b9f95513b9d62e9f3142ad2cb1a2e
Alex Paredes <alexparedesr95@gmail.com>
2022-03-24 12:16:21 -0600

    FEAT: Created loading message and an state to check if user is loggedIn or not.

--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { firebase } from '../firebase/firebase-config';
 import { JournalScreen } from '../components/journal/JournalScreen';
@@ -10,17 +10,31 @@ export const AppRouter = () => {
 
     const dispatch = useDispatch();
 
+    const [checking, setChecking] = useState( true );
+    const [isLoggedIn, setIsLoggedIn] = useState( false );
+
     useEffect(() => {
 
         firebase.auth().onAuthStateChanged( ( user ) => {
 
             if ( user?.uid ) {
                 dispatch( login( user.uid, user.displayName ) );
+                setIsLoggedIn( true );
+            } else {
+                setIsLoggedIn( false );
             }
+
+            setChecking( false );
 
         });
 
-    }, [ dispatch ]);
+    }, [ dispatch, setChecking, setIsLoggedIn ]);
+
+    if ( checking ) {
+        return (
+            <h1>Espere un momento....</h1>
+        );
+    }
     
 
     return (
