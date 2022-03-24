665d0e66598a949768ee3c90a7a207ebd98cc770
Alex Paredes <alexparedesr95@gmail.com>
2022-03-24 17:12:35 -0600

    FEAT: Added notesActive case to notesReducer.

--- src/reducers/notesReducer.js
+++ src/reducers/notesReducer.js
@@ -1,3 +1,5 @@
+import { types } from "../types/types";
+
 const initialstate = {
     notes: [],
     active: null
@@ -6,6 +8,14 @@ const initialstate = {
 export const notesReducer = ( state = initialstate, action ) => {
 
     switch ( action.type ) {
+
+        case types.notesActive:
+            return {
+                ...state,
+                active: {
+                    ...action.payload
+                }
+            };
     
         default:
             return state;
