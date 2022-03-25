df813afa18e9b96da0f2016e78e9e43fb84f0256
Alex Paredes <alexparedesr95@gmail.com>
2022-03-25 12:16:53 -0600

    FEAT: Added ability to edit note, dispatched noted changes.

--- src/components/notes/NoteScreen.jsx
+++ src/components/notes/NoteScreen.jsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useRef } from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { activeNote } from '../../actions/notes';
 import { useForm } from '../../hooks/useForm';
 import { NotesAppBar } from './NotesAppBar';
 
 export const NoteScreen = () => {
+
+    const dispatch = useDispatch();
 
     const { active:note } = useSelector( state => state.notes );
     const [ formValues, handleInputChange, reset ] = useForm( note );
@@ -14,12 +17,19 @@ export const NoteScreen = () => {
 
     useEffect(() => {
 
-        if ( note.id !== activeId ) {
+        if ( note.id !== activeId.current ) {
             reset( note );
             activeId.current = note.id;
         }
 
     }, [ note, reset ]);
+
+    useEffect(() => {
+        
+        dispatch( activeNote( formValues, { ...formValues } ) );
+
+    }, [ formValues, dispatch ]);
+    
     
 
     return (
@@ -31,6 +41,7 @@ export const NoteScreen = () => {
                     <input
                         type="text"
                         placeholder='Some awesome title'
+                        name="title"
                         className='notes__title-input'
                         autoComplete='off'
                         value={ title }
@@ -38,6 +49,7 @@ export const NoteScreen = () => {
                     />
                     <textarea
                         placeholder='What happened today?'
+                        name="body"
                         className='notes__textarea'
                         value={ body }
                         onChange={ handleInputChange }
