0cd9a9f5c28abef81b8f125889b50c5d0dfa4067
Alex Paredes <alexparedesr95@gmail.com>
2022-03-24 21:03:26 -0600

    FEAT: Added the ability to show the note values and to show the selected entry on change.

--- src/components/notes/NoteScreen.jsx
+++ src/components/notes/NoteScreen.jsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import { useSelector } from 'react-redux';
+import { useForm } from '../../hooks/useForm';
 import { NotesAppBar } from './NotesAppBar';
 
 export const NoteScreen = () => {
+
+    const { active:note } = useSelector( state => state.notes );
+    const [ formValues, handleInputChange, reset ] = useForm( note );
+
+    const { title, body } = formValues;
+
+    const activeId = useRef( note.id );
+
+    useEffect(() => {
+
+        if ( note.id !== activeId ) {
+            reset( note );
+            activeId.current = note.id;
+        }
+
+    }, [ note, reset ]);
+    
+
     return (
         <div className='notes__main-content'>
             <NotesAppBar />
@@ -13,17 +33,24 @@ export const NoteScreen = () => {
                         placeholder='Some awesome title'
                         className='notes__title-input'
                         autoComplete='off'
+                        value={ title }
+                        onChange={ handleInputChange }
                     />
                     <textarea
                         placeholder='What happened today?'
                         className='notes__textarea'
+                        value={ body }
+                        onChange={ handleInputChange }
                     ></textarea>
-                    <div className="notes__image">
-                        <img
-                            src="https://i1.wp.com/www.lafosadelrancor.com/wp-content/uploads/2019/07/Sith-Trooper-El-Ascenso-de-Skywalker.jpg?fit=678%2C489&ssl=1"
-                            alt="imagen"
-                        />
-                    </div>
+                    {
+                        ( note.url ) &&
+                        <div className="notes__image">
+                            <img
+                                src="https://i1.wp.com/www.lafosadelrancor.com/wp-content/uploads/2019/07/Sith-Trooper-El-Ascenso-de-Skywalker.jpg?fit=678%2C489&ssl=1"
+                                alt="imagen"
+                            />
+                        </div>
+                    }
                 </form>
             </div>
         </div>
