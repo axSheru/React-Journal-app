101dce882094ab50eded1e145ae9a43ba56f74a3
Alex Paredes <alexparedesr95@gmail.com>
2022-03-25 23:10:21 -0600

    FEAT: Added file input selector and dispatch the action to upload the selected file.

--- src/components/notes/NotesAppBar.jsx
+++ src/components/notes/NotesAppBar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { startSaveNote } from '../../actions/notes';
+import { startSaveNote, startUploadingFile } from '../../actions/notes';
 
 export const NotesAppBar = () => {
 
@@ -11,11 +11,35 @@ export const NotesAppBar = () => {
         dispatch( startSaveNote( note ) );
     };
 
+    const handlePictureUpload = () => {
+        document.querySelector( '#fileSelector' ).click();
+    };
+
+    const handleFileChange = ( e ) => {
+        const file = e.target?.files[0];
+
+        if ( file ) {
+            dispatch( startUploadingFile( file ) );
+        }
+    };
+
     return (
         <div className='notes__appbar'>
             <span>22 de marzo de 2022</span>
+            <input
+                id='fileSelector'
+                type="file"
+                name='file'
+                style={{ display: 'none' }}
+                onChange={ handleFileChange }
+            />
             <div>
-                <button className='btn'>Picture</button>
+                <button
+                    className='btn'
+                    onClick={ handlePictureUpload }
+                >
+                    Picture
+                </button>
                 <button
                     className='btn'
                     onClick={ handleSave }
