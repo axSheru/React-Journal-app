b53cbebde16bafd050552969dedea39647585801
Alex Paredes <alexparedesr95@gmail.com>
2022-04-25 20:59:35 -0500

    CHORE: Simplified firebase config using env variables.

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -4,28 +4,16 @@ import 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCRCIaHzR-U68ssOrayhJ9PLA_w188lWBo",
-  authDomain: "react-journal-app-550fc.firebaseapp.com",
-  projectId: "react-journal-app-550fc",
-  storageBucket: "react-journal-app-550fc.appspot.com",
-  messagingSenderId: "518754417397",
-  appId: "1:518754417397:web:32259635d4a5c4dacf63bc"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
-const firebaseConfigTesting = {
-  apiKey: "AIzaSyAhYGdrY9O_b9zHHNbHRhlI67BHzZpLypI",
-  authDomain: "journal-testing-50809.firebaseapp.com",
-  projectId: "journal-testing-50809",
-  storageBucket: "journal-testing-50809.appspot.com",
-  messagingSenderId: "110314677172",
-  appId: "1:110314677172:web:2358dc0261ac614e9448e1"
-};
-
-// Initialize Firebase conditionally by checking the environment.
-
-( process.env.NODE_ENV === 'test' )
-  ? firebase.initializeApp( firebaseConfigTesting )
-  : firebase.initializeApp( firebaseConfig );
+// Initialize Firebase.
+firebase.initializeApp( firebaseConfig );
 
 
 const db = firebase.firestore();
