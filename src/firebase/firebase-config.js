1c12f9360eb4acab8b3cf47e40c8ced61c512c73
Alex Paredes <alexparedesr95@gmail.com>
2022-04-25 20:22:28 -0500

    CHORE: Conditionally uses firebase configuration checking the environment.

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -12,8 +12,21 @@ const firebaseConfig = {
   appId: "1:518754417397:web:32259635d4a5c4dacf63bc"
 };
 
-// Initialize Firebase
-firebase.initializeApp( firebaseConfig );
+const firebaseConfigTesting = {
+  apiKey: "AIzaSyAhYGdrY9O_b9zHHNbHRhlI67BHzZpLypI",
+  authDomain: "journal-testing-50809.firebaseapp.com",
+  projectId: "journal-testing-50809",
+  storageBucket: "journal-testing-50809.appspot.com",
+  messagingSenderId: "110314677172",
+  appId: "1:110314677172:web:2358dc0261ac614e9448e1"
+};
+
+// Initialize Firebase conditionally by checking the environment.
+
+( process.env.NODE_ENV === 'test' )
+  ? firebase.initializeApp( firebaseConfigTesting )
+  : firebase.initializeApp( firebaseConfig );
+
 
 const db = firebase.firestore();
 const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
