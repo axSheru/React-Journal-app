06f94da15ecae5ada1efb3e157420d697ebeefb0
Alex Paredes <alexparedesr95@gmail.com>
2022-04-26 22:07:05 -0500

    TEST: Created test to check the error box shows with an error.

--- src/tests/components/auth/RegisterScreen.spec.js
+++ src/tests/components/auth/RegisterScreen.spec.js
@@ -4,6 +4,7 @@ import { MemoryRouter } from 'react-router-dom';
 import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import { RegisterScreen } from '../../../components/auth/RegisterScreen';
+import { types } from '../../../types/types';
 
 describe('Pruebas en el componente RegisterScreen.', () => {
 
@@ -11,6 +12,7 @@ describe('Pruebas en el componente RegisterScreen.', () => {
     const mockStore = configureStore(middlewares);
 
     const initState = {
+        auth: {},
         ui: {
             loading: false,
             msgError: null
@@ -18,8 +20,7 @@ describe('Pruebas en el componente RegisterScreen.', () => {
     };
 
     let store = mockStore( initState );
-
-    store.dispatch = jest.fn();
+    // store.dispatch = jest.fn();
 
     const wrapper = mount(
         <Provider store={ store }>
@@ -32,6 +33,59 @@ describe('Pruebas en el componente RegisterScreen.', () => {
     test('Debe de hacer match con el snapshot.', () => {
 
         expect( wrapper ).toMatchSnapshot();
+
+    });
+
+    /* test(Debe de ejecutarse la acción setError.', () => {
+
+        const emailField = wrapper.find( 'input[name="email"]' );
+        
+        
+        emailField.simulate( 'change', {
+            target: {
+                value: '',
+                name: 'email'
+            }
+        });
+
+        wrapper.find( 'form' ).simulate( 'submit', {
+            preventDefault(){}
+        });
+
+        const actions = store.getActions();
+
+        console.log(actions)
+
+        expect( actions[0] ).toEqual({
+            type: types.uiSetError,
+            payload: 'Email not valid.'
+        });
+
+    }); */
+
+    test('Debe de mostrar la caja de alerta con el error.', () => {
+
+        const initState = {
+            auth: {},
+            ui: {
+                loading: false,
+                msgError: 'Email no válido'
+            }
+        };
+    
+        const store = mockStore( initState );
+        // store.dispatch = jest.fn();
+    
+        const wrapper = mount(
+            <Provider store={ store }>
+                <MemoryRouter>
+                    <RegisterScreen />
+                </MemoryRouter>
+            </Provider>
+        );
+
+        expect( wrapper.find( '.auth__alert-error' ).exists() ).toBeTruthy();
+        expect( wrapper.find( '.auth__alert-error' ).text().trim() ).toBe( initState.ui.msgError );
 
     });
     
